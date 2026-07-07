c174f71bd77fc777a3f6defa212672f7f1f1f781
Jefferson Soares <oito7digital@gmail.com>
2026-07-07 09:41:17 -0300

    remove: debug-env endpoint temporario

--- app/api/debug-env/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-export async function GET() {
-  return NextResponse.json({
-    resend_key_exists: !!process.env.RESEND_API_KEY,
-    resend_key_prefix: process.env.RESEND_API_KEY?.slice(0, 8) ?? "VAZIO",
-  });
-}
